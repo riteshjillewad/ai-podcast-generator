f162e8d38c35f520b6e7ae243079bad954738b45
Ritesh Jillewad <130166456+riteshjillewad@users.noreply.github.com>
2026-04-22 11:45:16 +0530

    Update script_service.py

--- services/script_service.py
+++ services/script_service.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 
 
 def generate_dialogue(topic):
@@ -22,6 +22,91 @@ GUEST: line here
 No title.
 No notes.
 No extra text.
+"""
+
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "mistral",
+            "prompt": prompt,
+            "stream": False
+        }
+    )
+
+    return response.json()["response"].strip()
+'''
+
+import requests
+
+
+def generate_dialogue(topic, mode):
+
+    if mode == "debate":
+
+        prompt = f"""
+Create a podcast debate between HOST and GUEST.
+
+Topic: {topic}
+
+HOST challenges ideas.
+GUEST gives arguments.
+
+6 lines only.
+
+Format:
+HOST:
+GUEST:
+"""
+
+    elif mode == "motivational":
+
+        prompt = f"""
+Create an inspiring podcast between HOST and GUEST.
+
+Topic: {topic}
+
+HOST asks uplifting questions.
+GUEST gives motivational answers.
+
+6 lines only.
+
+Format:
+HOST:
+GUEST:
+"""
+
+    elif mode == "news":
+
+        prompt = f"""
+Create a podcast news discussion between HOST and GUEST.
+
+Topic: {topic}
+
+HOST presents headlines.
+GUEST gives insights.
+
+6 lines only.
+
+Format:
+HOST:
+GUEST:
+"""
+
+    else:
+
+        prompt = f"""
+Create an interview podcast between HOST and GUEST.
+
+Topic: {topic}
+
+HOST asks engaging questions.
+GUEST gives expert answers.
+
+6 lines only.
+
+Format:
+HOST:
+GUEST:
 """
 
     response = requests.post(
